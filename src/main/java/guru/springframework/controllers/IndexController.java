767263ac8f90132a5876cd0bb6f082c13bfaa3ff
Mario Serrano <maseflores79@gmail.com>
2018-11-25 16:34:26 -0700

    Conociendo los metodos JPA Query de Spring data

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,40 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 
 
 @Controller
 public class IndexController {
+	
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
 
-    @RequestMapping({"", "/", "/index"})
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
+
+	@RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
         // System.out.println("1 2 3 4... ");
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> uof = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+		System.out.println("Cat Id is : " + categoryOptional.get().getId());
+		System.out.println("UOF Id is : " + uof.get().getId());
+		
         return "index";
     }
 }
